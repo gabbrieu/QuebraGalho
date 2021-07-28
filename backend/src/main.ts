457ff37cd260f0da25d64414b5178ac01e6f88a4
gabbrieu <gabrielhmendes@hotmail.com>
2021-07-28 18:19:42 -0300

    feat: adding basic swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,17 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('API QuebraGalho')
+    .setDescription('API das rotas do QuebraGalho')
+    .setVersion('1.0')
+    .addTag('quebra-galho')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
