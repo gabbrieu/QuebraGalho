8aab79f0f96b5b393999495b7dd80b5c0a414cc7
ThiagooKings <thiagomreeis@gmail.com>
2021-08-04 16:08:45 -0300

    Home page finished

---
+++ frontend/quebragalho-site/src/components/Rodape.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/components/Rodape.module.scss'
+
+export function Rodape() {
+return(
+    <div className={styles.rodapeBg}>
+        <span> <a href ="https://github.com/gabbrieu/QuebraGalho/tree/develop" target="_blank" rel="noreferrer">Projeto Quebra Galho </a> Todos os direitos reservados © </span>
+    </div>
+
+);
+
+}
