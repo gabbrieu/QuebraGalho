765d88cbeebcd037c6306c4cb03dfcdc5fa15f17
Thiago Reis <thiagomreeis@gmail.com>
2021-11-15 21:05:02 -0300

    Ending service page

--- frontend/quebragalho-site/src/pages/servico.tsx
+++ frontend/quebragalho-site/src/pages/servico.tsx
@@ -46,6 +46,67 @@ return(
                   <button><span>Combinar Serviço</span><img src="icons/iconCombinarButton.svg" /></button>
                 </div>
               </div>
+              <h2>Depoimentos dos clientes</h2>
+              <div className={styles.boxDepoimentos}>
+                <div className={styles.cabecalhoDepoimentos}>
+                  <span> Júlia Alves </span>
+                  <img src ="icons/iconCombinarButton.svg"/>
+                  <span> José Luiz </span>
+                </div>
+                <div className={styles.conteudoDepoimento}>
+                  <div className={styles.fotoPerfilDep}>
+                    <img src = "img/content/fotoPerfilJ.png"/>
+                  </div>
+                 <div className={styles.camposDepoimento}>
+                    <h2>Júlia Alves</h2>
+                    <p> Trabalho muito bem feito, recomendo para todas as pessoas que precisam de serviço de pedreiro! </p>
+                    <div className={styles.notaDepoimento}>
+                      <span> Nota: </span>
+                      <img src="img/content/staricon.svg" />
+                    </div>
+                 </div>
+                </div>
+              </div>
+              <div className={styles.boxDepoimentos}>
+                <div className={styles.cabecalhoDepoimentos}>
+                  <span> Rita Carvalho </span>
+                  <img src ="icons/iconCombinarButton.svg"/>
+                  <span> José Luiz </span>
+                </div>
+                <div className={styles.conteudoDepoimento}>
+                  <div className={styles.fotoPerfilDep}>
+                    <img src = "img/content/fotoPerfilR.png"/>
+                  </div>
+                 <div className={styles.camposDepoimento}>
+                    <h2>Rita Carvalho</h2>
+                    <p> Trabalho muito bacana. Construiu minha casa bem rápido e por um preço bem acessível. </p>
+                    <div className={styles.notaDepoimento}>
+                      <span> Nota: </span>
+                      <img src="img/content/staricon.svg" />
+                    </div>
+                 </div>
+                </div>
+              </div>
+              <div className={styles.boxDepoimentos}>
+                <div className={styles.cabecalhoDepoimentos}>
+                  <span> Thiago Carvalho </span>
+                  <img src ="icons/iconCombinarButton.svg"/>
+                  <span> José Luiz </span>
+                </div>
+                <div className={styles.conteudoDepoimento}>
+                  <div className={styles.fotoPerfilDep}>
+                    <img src = "img/content/fotoPerfilT.png"/>
+                  </div>
+                 <div className={styles.camposDepoimento}>
+                    <h2>Thiago Carvalho</h2>
+                    <p> Ótimo profissional, trabalho maravilhoso. </p>
+                    <div className={styles.notaDepoimento}>
+                      <span> Nota: </span>
+                      <img src="img/content/staricon.svg" />
+                    </div>
+                 </div>
+                </div>
+              </div>
           </div>
         </div>
       </div>
